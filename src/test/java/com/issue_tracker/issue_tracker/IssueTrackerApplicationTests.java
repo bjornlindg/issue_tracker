24f181d0206b731155ef4a669af6d54708653b85
bjornlindg <lindgren.bjorn@gmail.com>
2025-01-03 15:06:32 +0100

    setup project

---
+++ src/test/java/com/issue_tracker/issue_tracker/IssueTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.issue_tracker.issue_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
