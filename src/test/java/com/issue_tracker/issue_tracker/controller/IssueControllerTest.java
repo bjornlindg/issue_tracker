2d99a4a58c2628aaacdb40df9a8db08380f61b5d
bjornlindg <lindgren.bjorn@gmail.com>
2025-02-08 10:06:19 +0100

    Added unit test for the IssueController class

---
+++ src/test/java/com/issue_tracker/issue_tracker/controller/IssueControllerTest.java
@@ -0,0 +1,49 @@
+package com.issue_tracker.issue_tracker.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.issue_tracker.issue_tracker.model.Issue;
+import com.issue_tracker.issue_tracker.service.IssueService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(IssueController.class)
+class IssueControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private IssueService issueService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testGetAllIssues() throws Exception {
+        Issue issue1 = new Issue("Issue 1", "Description 1", "Open");
+        Issue issue2 = new Issue("Issue 2", "Description 2", "Closed");
+
+        when(issueService.getAllIssues()).thenReturn(Arrays.asList(issue1, issue2));
+
+        mockMvc.perform(get("/api/issues"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].title").value("Issue 1"));
+    }
+}
